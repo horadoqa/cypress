5684b0e60dac8a370cd42a09ea5697d766485e9f
Ricardo Fahham <rfahham@hotmail.com>
2024-09-17 18:11:00 -0300

    falta só a validação

--- cypress/e2e/youtube/youtube.cy.js
+++ cypress/e2e/youtube/youtube.cy.js
@@ -1,5 +1,23 @@
-describe('Youtube', () => {
-    it('passes', () => {
-      cy.visit('https://www.youtube.com')
+describe('Acessar site do Youtube', () => {
+    
+    it('Acessar o site', () => {
+      
+      cy.visit('https://www.youtube.com', {timeout: 10000})
+
+      cy.get('#search-input').type('Positive Jazz - Smooth Piano Jazz Music & Relaxing September Bossa Nova instrumental for Good mood')
+
+      cy.get('#search-icon-legacy').click()
+
+
     })
+
+    // Funcionando !!!
+
+    it('Validate', () => {
+    
+      cy.visit('https://www.youtube.com/watch?v=-ZW0vCYiKGM')
+    
+      cy.get('h1').should('contains.text', 'Positive Jazz - Smooth Piano Jazz Music & Relaxing September Bossa Nova instrumental for Good mood')
+    });
+
   })
